25d21582e49eae9bf9e67b320eddcf0f42f77ebe
bhavna <soni304bhavna@gmail.com>
2024-06-06 19:39:58 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Function to add two numbers
+function sum(a, b) {
+    return a + b;
+  }
+  
+  // Example usage
+  let number1 = 5;
+  let number2 = 10;
+  let result = sum(number1, number2);
+  
+  console.log("The sum is: " + result);
+  
